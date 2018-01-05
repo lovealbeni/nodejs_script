c8c5b6e91d10d4e7096da7fb328bcca0896c8e9c
albeni <785762771@qq.com>
2018-01-05 13:49:13 +0800

    add a new script

---
+++ remTorpx.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const file = process.argv[2];
+const fs = require('fs');
+
+function getFileExt(file) {
+	return path.extname(file);
+}
+
+function main(){
+	if(!file){
+		throw new Error('没有指定wxss文件');
+	}
+	if(getFileExt(file).slice(1).toLowerCase() !== 'wxss'){
+		throw new Error('指定的文件格式不正确');
+	}
+	let wxssFile = path.resolve(process.cwd(),file);
+	let filestream = fs.readFileSync(wxssFile).toString();
+	let reg = /([0-9]+)[.]([0-9]?)rem/g;
+	console.log(filestream.replace(reg, function(m,p1,p2){
+		let tempNumber = p1+'.'+p2;
+		tempNumber = parseFloat(tempNumber)*100;
+		return tempNumber+'rpx';
+	}));
+}
+
+main();
