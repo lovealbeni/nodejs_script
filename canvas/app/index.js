ad0a33f28f8009966d9b715a48e1273bd0180d1e
zhengshibin <785762771@qq.com>
2019-02-12 19:11:28 +0800

    add fabric webpack

---
+++ canvas/app/index.js
@@ -0,0 +1,26 @@
+import { fabric } from 'fabric';
+function main(){
+	let width = document.body.clientWidth;
+	let height = document.body.clientHeight;
+	let canvas = new fabric.Canvas('c');
+	canvas.setHeight(height*0.95);
+	canvas.setWidth(width);
+	let rect = new fabric.Rect({
+		left:100,
+		top:50,
+		fill:'#d81b60',
+		width:100,
+		height:100,
+		strokeWidth:2,
+		stroke: '#000000',
+		rx:10,
+		ry:10,
+		angle:45,
+		// hasControls:false,
+		lockMovementX:true,
+		lockMovementY:true,
+		hoverCursor:null
+	});
+	canvas.add(rect);
+}
+window.onload = main;
---
+++ canvas/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+	mode: 'development',
+	entry: './app/index.js',
+	output:{
+		filename:'bundle.js',
+		path: path.resolve(__dirname,'dist'),
+	}
+}
