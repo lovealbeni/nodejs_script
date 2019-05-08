cb39ba61f62c9693d8047a64280d75f1a734d74a
zhengshibin <785762771@qq.com>
2019-05-08 21:45:56 +0800

    add file

--- gif/app/index.js
+++ gif/app/index.js
@@ -4,33 +4,33 @@ import ThreeGif from './threeGif';
 function main(){
 	let gif = new ThreeGif();
 	gif.export();
-	// let maker = new TextColum({
-	// 	textArray:['123','456'],	
-	// 	width:1242,
-	// 	height:101,
-	// 	backgroundImg: './textBkg.jpg',
-	// 	gifConfig:{
-	// 		quality:1,
-	// 		debug:true
-	// 	}
-	// })
-	// let dom = document.getElementById('gif');
-	// maker.exportGif().then(src=>{
-	// 	dom.src = src;
-	// });
-	// let maker2 = new GifMaker({
-	// 	img: ['./1.png','./2.png','./3.png','./4.png','./2.png','./3.png'],
-	// 	width:621,
-	// 	height:292,
-	// 	backgroundImg:'./background.jpg',
-	// 	gifConfig:{
-	// 		debug:true
-	// 	}
-	// });
-	// let dom2 = document.getElementById('gif2');
-	// maker2.exportGif().then(src => {
-	// 	dom2.src = src;
-	// });
+	let maker = new TextColum({
+		textArray:['123','456'],	
+		width:1242,
+		height:101,
+		backgroundImg: './textBkg.jpg',
+		gifConfig:{
+			quality:1,
+			debug:true
+		}
+	})
+	let dom = document.getElementById('gif');
+	maker.exportGif().then(src=>{
+		dom.src = src;
+	});
+	let maker2 = new GifMaker({
+		img: ['./1.png','./2.png','./3.png','./4.png','./2.png','./3.png'],
+		width:621,
+		height:292,
+		backgroundImg:'./background.jpg',
+		gifConfig:{
+			debug:true
+		}
+	});
+	let dom2 = document.getElementById('gif2');
+	maker2.exportGif().then(src => {
+		dom2.src = src;
+	});
 
 	// new GifMaker({
 	// 	img: ['./1.png','./2.png','./3.png','./4.png','./2.png','./3.png'],
