7c6fa3c8f4465de82e7ee97222f117dcf146f763
albeni <zhengshibin@css.cn>
2019-09-27 17:37:10 +0800

    修改了文件的tsconfig,增加了promise和dom的支持，图片纹理的问题解决，todo：整理代码

--- webgl/app/index.ts
+++ webgl/app/index.ts
@@ -32,12 +32,13 @@ function main(){
         300,300,0,0,255,1,
         300,30,128,128,128,1
     ];
-    positions = prepareCirclePointsAndcolors({
-        x:400,
-        y:200,
-        r:100,
-        n:50
-    });
+    // positions = prepareCirclePointsAndcolors({
+    //     x:400,
+    //     y:200,
+    //     r:100,
+    //     n:50
+    // });
+    positions = prepareTexturePositions();
     var indices = [
         0,1,2,
         0,2,3
@@ -51,10 +52,33 @@ function main(){
     var type = gl.FLOAT;
     var normalize = false;
     var stride = 6*4; //一个顶点的数据量（2个坐标，4个颜色）
+    var textureStride = 4*4; // 纹理坐标的数据量（2个坐标，2个纹理坐标）
     var offset = 0;
+    var textureOffset = 2*4; //有两个坐标
     //  顶点属性指针
-    gl.vertexAttribPointer(a_Position,size,type,normalize,stride,offset);
+    gl.vertexAttribPointer(a_Position,size,type,normalize,textureStride,offset);
 
+    // 纹理坐标传入
+    var a_Uv = gl.getAttribLocation(program,'a_Uv');
+    gl.enableVertexAttribArray(a_Uv);
+    gl.vertexAttribPointer(a_Uv,size,type,normalize,textureStride,textureOffset);
+    // 初始化纹理
+    var uniformTexture = gl.getUniformLocation(program,'texture');
+    prepareTextureImg('./app/timg.jpg').then(img=>{
+        gl.activeTexture(gl.TEXTURE0);
+        var texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D,texture);
+        gl.texImage2D(gl.TEXTURE_2D,0,gl.RGBA,gl.RGBA,gl.UNSIGNED_BYTE,img);
+        gl.texParameterf(gl.TEXTURE_2D,gl.TEXTURE_MAG_FILTER,gl.LINEAR);
+        gl.texParameterf(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER,gl.LINEAR);
+        gl.uniform1i(uniformTexture,0);
+
+        var primitiveType = gl.TRIANGLE_FAN;
+        var offset = 0;
+        var count = 4;
+        count = positions.length/4;//和stride的值相等，生成的数组里面有6个坐标形成一个点，所以要/6
+        gl.drawArrays(primitiveType,offset,count);
+    });
     // 顶点buffer
     var indicesBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,indicesBuffer);
@@ -64,7 +88,7 @@ function main(){
     var a_Screen_size = gl.getAttribLocation(program,'a_Screen_size');
     gl.vertexAttrib2f(a_Screen_size,canvas.width,canvas.height);
     // 传颜色
-    var a_Color = gl.getAttribLocation(program,'a_Color');
+    // var a_Color = gl.getAttribLocation(program,'a_Color');
 
     // 把color 和 position 放在一个buffer里面
     // var colors = [
@@ -79,19 +103,19 @@ function main(){
     // gl.bindBuffer(gl.ARRAY_BUFFER,colorBuffer);
     // gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(colors),gl.STATIC_DRAW);
     
-    gl.enableVertexAttribArray(a_Color);
-    var colorSize = 4;
-    var colorOffset = 2*4;//color的偏移量= position's size
-    gl.vertexAttribPointer(a_Color,colorSize,type,normalize,stride,colorOffset);
+    // gl.enableVertexAttribArray(a_Color);
+    // var colorSize = 4;
+    // var colorOffset = 2*4;//color的偏移量= position's size
+    // gl.vertexAttribPointer(a_Color,colorSize,type,normalize,stride,colorOffset);
 
     gl.clearColor(0.0,0.0,0.0,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    var primitiveType = gl.TRIANGLE_FAN;
-    var offset = 0;
-    var count = 4;
-    count = positions.length/6;//和stride的值相等，生成的数组里面有6个坐标形成一个点，所以要/6
-    gl.drawArrays(primitiveType,offset,count);
+    // var primitiveType = gl.TRIANGLE_FAN;
+    // var offset = 0;
+    // var count = 4;
+    // count = positions.length/6;//和stride的值相等，生成的数组里面有6个坐标形成一个点，所以要/6
+    // gl.drawArrays(primitiveType,offset,count);
     // gl.drawElements(primitiveType,count,gl.UNSIGNED_SHORT,offset)
 
 
@@ -135,4 +159,27 @@ function prepareCirclePointsAndcolors({x,y,r,n}){
         pointsAndColors.push(x+r*cos(i*(pi/n)),y+r*sin(i*(pi/n)),255,0,0,1);
     }
     return pointsAndColors;
+}
+
+function prepareTexturePositions(){
+    var positions = [
+        30,30,0,0,
+        30,300,0,1,
+        300,300,1,1,
+        30,30,0,0,
+        300,300,1,1,
+        300,30,1,0
+    ];
+    return positions;
+}
+
+function prepareTextureImg(url:String){
+    // 准备纹理贴图的
+    return new Promise((resove,reject)=>{
+        var img = new Image();
+        img.onload = function(){
+            resove(img);
+        };
+        img.src = url.toString();
+    });
 }
