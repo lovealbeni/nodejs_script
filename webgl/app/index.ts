7e34acf50f5c71d56fe72e4009d0b24698dd86b6
albeni <zhengshibin@css.cn>
2019-10-09 11:37:20 +0800

    change function call relationship

--- webgl/app/index.ts
+++ webgl/app/index.ts
@@ -2,16 +2,16 @@ import Matrix from './cuon-matrix';
 import { textureMain } from '../demo/texture';
 import { circleMain } from '../demo/circle';
 function main(){
+    var functionMap = {
+        circle:circleMain,
+        texture:textureMain
+    };
     var canvas = document.createElement('canvas');
     canvas.width = document.body.clientWidth;
     canvas.height = canvas.width * 0.6;
+    var drawType = 'texture';
 
-    // ----- circle main ------
-    circleMain(canvas);
-
-
-    // ------ texture main -----
-    // textureMain(canvas);
+    functionMap[drawType](canvas);
 
 }
 window.onload = main;
