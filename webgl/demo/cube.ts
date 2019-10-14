2497125bb8b15f98316b4533f6b9940e392a163e
albeni <zhengshibin@css.cn>
2019-10-14 16:04:43 +0800

    修改方法，变成通过geo下面的class生成图形，实现了多个图形同时加载出来

--- webgl/demo/cube.ts
+++ webgl/demo/cube.ts
@@ -2,12 +2,13 @@ import { drawInterface,initWebgl,attach } from './util';
 import { identity,multiply,rotateX,rotateY,ortho } from './matrix';
 import fshaderSource from '../app/sl/cubeFshader.glsl';
 import vshaderSource from '../app/sl/cubeVshader.glsl';
+import { Cube as cubeClass } from './geo/cubeClass';
 
 function cubeMain(canvas:HTMLCanvasElement){
     var { gl,program } = initWebgl(canvas,vshaderSource,fshaderSource);
     gl.clearColor(0.0,0.0,0.0,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    drawCubeMain({
+    drawCubeMultiMain({
         gl:gl,
         program:program,
         canvas:canvas
@@ -15,27 +16,29 @@ function cubeMain(canvas:HTMLCanvasElement){
     attach(canvas);
 }
 
-
-function drawCubeMain(rederObj:drawInterface){
+function drawCubeMultiMain(rederObj:drawInterface){
     let {gl,program,canvas} = rederObj;
-    let { colors,indices,positions } = createCube(1,1,1);
-    console.log({colors,indices,positions});
+    let cubes:cubeClass[] = [];
+    cubes.push(new cubeClass(0,0,0));
+    cubes.push(new cubeClass(1,1,0));
+    cubes.push(new cubeClass(0,2,0));
+
     var a_Position = gl.getAttribLocation(program,'a_Position');
     var positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER,positionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER,positions,gl.STATIC_DRAW);
     gl.enableVertexAttribArray(a_Position);
     var positionSize = 3;
     var positionType = gl.FLOAT;
     var positionNormalize = false;
     var positionStride = 3*4;
     var positionOffset = 0;
+    // 这里要特殊注意一下，js的执行时根据当前的情况来默认绑定的，也就是说，执行vertexAttribPointer的时候，会把当前的
+    // attribute和buffer绑定在一起，反例可以将下面代码复制到colorbuffer的vertex上方，就会发现不一样的情况
     gl.vertexAttribPointer(a_Position,positionSize,positionType,positionNormalize,positionStride,positionOffset);
 
     var a_Color = gl.getAttribLocation(program,'a_Color');
     var colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER,colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER,colors,gl.STATIC_DRAW);
     gl.enableVertexAttribArray(a_Color);
     var colorSize = 4;
     var colorType = gl.FLOAT;
@@ -46,97 +49,40 @@ function drawCubeMain(rederObj:drawInterface){
 
     var indicesBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,indicesBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,indices,gl.STATIC_DRAW);
 
     var u_Matrix = gl.getUniformLocation(program,'u_Matrix');
-
+    let aspect = canvas.width/canvas.height;
+    var projectionMatrix = ortho(-aspect*4,aspect*4,-4,4,100,-100);
+    var curMatrix = identity();
+    curMatrix = multiply(projectionMatrix,curMatrix);
 
     gl.enable(gl.CULL_FACE);
 
-    gl.drawElements(gl.TRIANGLES,indices.length,gl.UNSIGNED_SHORT,0);
-    setTimeout(()=>{
-        let curMatrix = identity();
-        animate(gl,1,1,u_Matrix,curMatrix,indices.length,canvas,true);
-    },200);
+    function drawRotateCube(){
+        var xAngle = Math.random();
+        var yAngle = Math.random();
+        curMatrix = rotateX(curMatrix,deg2radians(xAngle));
+        curMatrix = rotateY(curMatrix,deg2radians(yAngle));
+        
+        gl.uniformMatrix4fv(u_Matrix,false,curMatrix);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        for(let i = 0;i<cubes.length;i++){
+            let nowCube = cubes[i];
+            gl.bindBuffer(gl.ARRAY_BUFFER,positionBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER,nowCube.getPositions(),gl.STATIC_DRAW);
+            gl.bindBuffer(gl.ARRAY_BUFFER,colorBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER,nowCube.getColors(),gl.STATIC_DRAW);
+            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,nowCube.getIndices(),gl.STATIC_DRAW);
+            gl.drawElements(gl.TRIANGLES,nowCube.getIndices().length,gl.UNSIGNED_SHORT,0);
+        }
+        requestAnimationFrame(drawRotateCube);
+    }
+    drawRotateCube();
 }
 
 function deg2radians(angle){
     return (angle * Math.PI)/180;
 }
-
-function animate(gl:WebGLRenderingContext,xAngle:number,yAngle:number,u_Matrix,curMatrix,length,canvas:HTMLCanvasElement,isFirst:boolean){
-    gl.uniformMatrix4fv(u_Matrix,false,curMatrix);
-    curMatrix = rotateX(curMatrix,deg2radians(xAngle));
-    curMatrix = rotateY(curMatrix,deg2radians(yAngle));
-    if(isFirst){
-        let aspect = canvas.width/canvas.height;
-        var projectionMatrix = ortho(-aspect*4,aspect*4,-4,4,100,-100);
-        curMatrix = multiply(projectionMatrix,curMatrix);
-    }
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawElements(gl.TRIANGLES,length,gl.UNSIGNED_SHORT,0);
-    requestAnimationFrame(function(){
-        animate(gl,xAngle,yAngle,u_Matrix,curMatrix,length,canvas,false);
-    });
-    // setTimeout(() => {
-    //     animate(gl,xAngle,yAngle,u_Matrix,curMatrix,length,canvas,false);
-    // }, 1000);
-}
-
-function createCube(width:number,height:number,depth:number){
-    let zeroX = width/2 , zeroY = height/2 , zeroZ = depth/2;
-    let cornerPosition = [
-        [-zeroX, -zeroY, -zeroZ],
-        [zeroX, -zeroY, -zeroZ],
-        [zeroX, zeroY, -zeroZ],
-        [-zeroX, zeroY, -zeroZ],
-        [-zeroX, -zeroY, zeroZ],
-        [-zeroX, zeroY, zeroZ],
-        [zeroX, zeroY, zeroZ],
-        [zeroX, -zeroY, zeroZ]
-    ];//一个正方形所有的顶点坐标，每个坐标有x,y,z三个分量
-    const CUBE_FACE_INDICES = [
-        [0, 1, 2, 3], //前面
-        [4, 5, 6, 7], //后面
-        [0, 3, 5, 4], //左面
-        [1, 7, 6, 2], //右面
-        [3, 2, 6, 5], //上面
-        [0, 4, 7, 1] // 下面
-    ];
-    const FACE_COLORS = [
-        [1, 0, 0, 1], // 前面，红色
-        [0, 1, 0, 1], // 后面，绿色
-        [0, 0, 1, 1], // 左面，蓝色
-        [1, 1, 0, 1], // 右面，黄色
-        [1, 0, 1, 1], // 上面，品色
-        [0, 1, 1, 1]  // 下面，青色
-    ];
-    let colors = [];
-    let positions = [];
-    let indices = [];
-    for(let f = 0; f < 6;++f){
-        let faceIndices = CUBE_FACE_INDICES[f]; //每一个面的索引
-        let color = FACE_COLORS[f];
-        for(let v = 0; v < 4;++v){
-            let position = cornerPosition[faceIndices[v]];
-            positions = positions.concat(position);
-            colors = colors.concat(color);
-        }
-        let offset = 4*f;
-        indices.push(offset+0,offset+1,offset+2);
-        indices.push(offset+0,offset+2,offset+3);
-    }
-    let fcolors = new Float32Array(colors);
-    let fpositions = new Float32Array(positions);
-    let findices = new Uint16Array(indices);
-
-    return {
-        positions:fpositions,
-        colors:fcolors,
-        indices:findices
-    }
-}
-
 export {
     cubeMain
 }
---
+++ webgl/demo/geo/cubeClass.ts
@@ -0,0 +1,86 @@
+class Cube{
+    x:number;
+    y:number;
+    z:number;
+    static width:number = 1;
+    static height:number = 1;
+    static depth:number = 1;
+    positions:Float32Array;
+    indices:Uint16Array;
+    colors:Float32Array;
+    constructor(x:number,y:number,z:number){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        let {positions,colors,indices} = this.createCube(this.x,this.y,this.z);
+        this.positions = positions;
+        this.colors = colors;
+        this.indices = indices;
+    }
+    getPositions():Float32Array{
+        return this.positions;
+    }
+    getIndices():Uint16Array{
+        return this.indices;
+    }
+    getColors():Float32Array{
+        return this.colors;
+    }
+    createCube(x:number,y:number,z:number){
+        let zeroX = Cube.width/2 , zeroY = Cube.height/2 , zeroZ = Cube.depth/2;
+        let cornerPosition = [
+            [x-zeroX, y-zeroY, z-zeroZ],
+            [x+zeroX, y-zeroY, z-zeroZ],
+            [x+zeroX, y+zeroY, z-zeroZ],
+            [x-zeroX, y+zeroY, z-zeroZ],
+            [x-zeroX, y-zeroY, z+zeroZ],
+            [x-zeroX, y+zeroY, z+zeroZ],
+            [x+zeroX, y+zeroY, z+zeroZ],
+            [x+zeroX, y-zeroY, z+zeroZ]
+        ];//一个正方形所有的顶点坐标，每个坐标有x,y,z三个分量
+        const CUBE_FACE_INDICES = [
+            [0, 1, 2, 3], //前面
+            [4, 5, 6, 7], //后面
+            [0, 3, 5, 4], //左面
+            [1, 7, 6, 2], //右面
+            [3, 2, 6, 5], //上面
+            [0, 4, 7, 1] // 下面
+        ];
+        const FACE_COLORS = [
+            [1, 0, 0, 1], // 前面，红色
+            [0, 1, 0, 1], // 后面，绿色
+            [0, 0, 1, 1], // 左面，蓝色
+            [1, 1, 0, 1], // 右面，黄色
+            [1, 0, 1, 1], // 上面，品色
+            [0, 1, 1, 1]  // 下面，青色
+        ];
+        let colors = [];
+        let positions = [];
+        let indices = [];
+        for(let f = 0; f < 6;++f){
+            let faceIndices = CUBE_FACE_INDICES[f]; //每一个面的索引
+            let color = FACE_COLORS[f];
+            for(let v = 0; v < 4;++v){
+                let position = cornerPosition[faceIndices[v]];
+                positions = positions.concat(position);
+                colors = colors.concat(color);
+            }
+            let offset = 4*f;
+            indices.push(offset+0,offset+1,offset+2);
+            indices.push(offset+0,offset+2,offset+3);
+        }
+        let fcolors = new Float32Array(colors);
+        let fpositions = new Float32Array(positions);
+        let findices = new Uint16Array(indices);
+    
+        return {
+            positions:fpositions,
+            colors:fcolors,
+            indices:findices
+        }
+    }
+}
+
+export {
+    Cube
+}
