5f577f6f2d26a8585ab53269febbffb1cd0277cb
albeni <zhengshibin@css.cn>
2019-10-22 19:05:00 +0800

    增加漫反射光照

--- webgl/app/sl/lightFshader.glsl
+++ webgl/app/sl/lightFshader.glsl
@@ -1,8 +1,15 @@
 precision mediump float;
+varying vec3 v_Normal;
 varying vec4 v_Color;
 uniform vec3 u_LightColor;
 uniform float u_AmbientFactor;
+uniform vec3 u_LightPosition;
 void main(){
-    vec3 anibientColor = u_AmbientFactor * u_LightColor;
-    gl_FragColor = vec4(anibientColor,1)*v_Color;
+    vec3 ambient = u_AmbientFactor * u_LightColor;
+    vec3 lightDirection = u_LightPosition - vec3(0,0,0);
+    float diffuseFactor = dot(normalize(lightDirection),normalize(v_Normal));
+    diffuseFactor = max(diffuseFactor,0.0);
+    vec3 diffuseLightColor = diffuseFactor * u_LightColor;
+    // 环境光部分加上反射光部分
+    gl_FragColor = v_Color * vec4((ambient + diffuseLightColor),1);
 }
--- webgl/app/sl/lightVshader.glsl
+++ webgl/app/sl/lightVshader.glsl
@@ -1,9 +1,12 @@
 precision mediump float;
 attribute vec3 a_Position;
 attribute vec4 a_Color;
+attribute vec3 a_Normal;
+varying vec3 v_Normal;
 varying vec4 v_Color;
 uniform mat4 u_Matrix;
 void main(){
     gl_Position = u_Matrix * vec4(a_Position,1);
     v_Color = a_Color;
+    v_Normal = a_Normal;
 }
