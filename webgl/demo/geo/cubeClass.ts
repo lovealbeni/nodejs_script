f1167a8c0856e62fcd596a6ed52a8f59436cec36
albeni <zhengshibin@css.cn>
2019-10-23 15:56:36 +0800

    修改法向量，成果没问题

--- webgl/demo/geo/cubeClass.ts
+++ webgl/demo/geo/cubeClass.ts
@@ -42,10 +42,10 @@ class Cube extends Geo {
         const normalInput = [
             [0, 0, 1],
             [0, 0, -1],
-            [-1, 0, 0],
             [1, 0, 0],
-            [0, 1, 0],
-            [0, -1, 0]
+            [-1, 0, 0],
+            [0, -1, 0],
+            [0, 1, 0]
         ];
         let colors = [];
         let positions = [];
