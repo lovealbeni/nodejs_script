9d8707b4cd90b32eae730413e16dc23c5436d3be
albeni <zhengshibin@css.cn>
2019-11-08 16:17:36 +0800

    add bind function

--- bind.js
+++ bind.js
@@ -1,3 +1,24 @@
-function mybind(context,fun){
-    
+Function.prototype.bind2 = function (context) {
+    var that = this;
+    var rFunction = function () {
+        that.apply(this instanceof that?this:context, arguments);
+    }
+    rFunction.prototype = this.prototype;
+    return rFunction;
 }
+
+function log() {
+    this.abc = 'name';
+    console.log(this.name);
+}
+
+var obj = {
+    name: 'obj bind name'
+}
+
+var logBind = log.bind2(obj);
+logBind();
+
+var entity = new logBind();
+
+console.log(entity.constructor);
