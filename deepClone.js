d864942681795906440f5ef64e7aaef5e6909078
albeni <zhengshibin@css.cn>
2019-11-08 09:48:03 +0800

    format code for deepclone

--- deepClone.js
+++ deepClone.js
@@ -1,22 +1,19 @@
-function clone(src,map = {}){
+function clone(src, map = {}) {
     let target = Array.isArray(src) ? [] : {};
-    for(let key in src){
-        if(typeof src[key] === 'object'){
-            if(map[src[key]]){
-                return map[src[key]];
-            }
-            target[key] = clone(src[key],map);
-            map[src[key]] = target;
-        }else{
-            target[key] = src[key];
+    for (let key in src) {
+        let source = src[key];
+        if (typeof source === 'object') {
+            target[key] = clone(source, map);
+        } else {
+            target[key] = source;
         }
     }
     return target;
 }
 
 var data = {
-    c:[1,2,3],
-    u:undefined
+    c: [1, 2, 3],
+    u: undefined
 }
 data.d = data;
 
@@ -24,7 +21,7 @@ var assign = {};
 
 assign = clone(data);
 
-console.log('assign',assign);
+console.log('assign', assign);
 
 assign.c.push(4);
 
