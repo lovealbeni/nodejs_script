2dca20aee05a479cd19e51244ea3e137d5aef33c
zhengshibin <zhengshibin@yy.com>
2020-07-05 18:02:29 +0800

    change shader

--- webgl/app/sl/shaderFshader.glsl
+++ webgl/app/sl/shaderFshader.glsl
@@ -1,12 +1,25 @@
 precision highp float;
 uniform vec2 u_Screen;
+varying vec3 v_Position;
+uniform float u_Time;
+const float PI = 3.1415926;
 
-void main() {
-    float y = mix(0., u_Screen.y, gl_FragCoord.x/u_Screen.x);
-    if(abs(y - gl_FragCoord.y) <= 1.){
-        gl_FragColor = vec4(0, 1, 1, 1);
-    } else {
-        gl_FragColor = vec4(0, 0, 0, 1);
+void main () {
+    // 将坐标系坐标范围约束在【-4，4】和【-5，3】之间。
+    vec2 pos = ( gl_FragCoord.xy / u_Screen.xy )*8.-vec2(4., 5.);
+    if(pos.y>-5.){
+        pos.y += 0.1 * sin(u_Time * 3.) + 0.13 * cos(u_Time * 2. + 0.6) + .1 * sin(u_Time * 3. + 0.4) + 0.2 * fract(sin(u_Time * 400.));
     }
-    return;
+    vec3 color = vec3(0., 0., 0.0);
+    float y = -pow(pos.x, 3.2)/(0.008) * 3.3;
+    //计算当前点与抛物线点的距离，并缩小距离范围。
+    float dir = length(pos-vec2(pos.x, y)) * sin(0.3);
+    if(dir < 0.7){
+        color.rg += smoothstep(0.0,1.,.75-dir);
+        //弱化绿色通道颜色
+        color.g /=2.4;
+    }
+    //强化红色通道颜色。
+    color += pow(color.r,1.1);
+    gl_FragColor = vec4(vec3(color) , 1.0 );
 }
--- webgl/demo/shader.ts
+++ webgl/demo/shader.ts
@@ -18,9 +18,18 @@ function drawShaderMain(renderObj:drawInterface){
     var { gl,program,canvas } = renderObj
     var a_Position = gl.getAttribLocation(program,'a_Position')
     var u_Screen = gl.getUniformLocation(program,'u_Screen')
+    var u_Time = gl.getUniformLocation(program,'u_Time')
     gl.vertexAttrib1f(a_Position,0.0)
     gl.uniform2f(u_Screen,canvas.width,canvas.height)
-    gl.drawArrays(gl.POINTS,0,1)
+    let time = 1.0
+    const drawLoop = () => {
+        gl.uniform1f(u_Time,time)
+        gl.drawArrays(gl.POINTS,0,1)
+        time += 0.01
+        time = time % 10
+        requestAnimationFrame(drawLoop)
+    }
+    requestAnimationFrame(drawLoop)
 }
 
 export {
